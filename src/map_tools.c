c4695149d9989c2a867cbdcf7e1ac9cdf9f6ef24
Iker Rozas <ikrozas@c3r4s1.42urduliz.com>
2025-06-12 16:00:29 +0200

    map_tools

---
+++ src/map_tools.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_tools.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikrozas <ikrozas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 15:44:28 by ikrozas           #+#    #+#             */
+/*   Updated: 2025/06/12 15:58:02 by ikrozas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	get_map_height(char	**map)
+{
+	int	i;
+
+	i = 0;
+	wile ( map[i])
+		i++;
+	return (i);
+}
+
+char	**copy_map(char **map)
+{
+	char	**new_map;
+	int		i;
+	int		height;
+
+	height = get_map_height(map);
+	new_map = malloc(sizeof(char *) * (height + 1));
+	if (!new_map)
+		return (NULL);
+	i = 0;
+	while (i < height)
+	{
+		new_map[i] = ft_strdup(map[i]);
+		if (!new_map[i])
+			return (NULL);
+		i++;
+	}
+	new_map[i] = NULL;
+	return (new_map);
+}
--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ikrozas <ikrozas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 14:55:04 by ikrozas           #+#    #+#             */
-/*   Updated: 2025/06/05 11:50:17 by ikrozas          ###   ########.fr       */
+/*   Updated: 2025/06/12 15:58:50 by ikrozas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,7 @@ char	**map_read(const char *file_path);
 int		is_rectangular(char **map);
 int		check_walls(char **map);
 int		check_parameters(char **map);
+char	**copy_map(char **map);
+int		get_map_height(char **map);
 
 #endif
